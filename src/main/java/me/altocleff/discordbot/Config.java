cc377881665a56ee46a2e2a3911ab36c5327074d
AltoCleff <nexus_grid@mail.ru>
2023-02-15 22:13:35 +0300

    add path for .env file, and fix pom.xml

--- src/main/java/me/altocleff/discordbot/Config.java
+++ src/main/java/me/altocleff/discordbot/Config.java
@@ -2,9 +2,27 @@ package me.altocleff.discordbot;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.net.URISyntaxException;
+
 public class Config {
 
     private static final Dotenv dotenv = Dotenv.load();
+
+    public Config () {
+        try {
+            String path = Application.class
+                    .getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .toURI()
+                    .getPath();
+
+            Dotenv.configure()
+                    .directory(path);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static String get(String key) {
         return dotenv.get(key.toUpperCase());
